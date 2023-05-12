4aac5652fe8a60010408c2675f88965593bca1cb
alessia <alessia.ciliberti@orbyta.it>
2023-05-12 11:58:44 +0200

    dipendenze

--- src/main/java/clinic/programming/traning/Application.java
+++ src/main/java/clinic/programming/traning/Application.java
@@ -1,14 +1,23 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String words){
+		String[] eparateWords = StringUtils.split(words,' ');
+		return (separateWords == null) ? 0 : separateWords.lenght;
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	int sount = app.countWords("I have four words");
+	System.out.println("Word Count: "+count);
     }
 }
